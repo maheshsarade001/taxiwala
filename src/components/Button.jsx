4e3710dfb875c56d696af2f4424e445f3fb8a274
maheshsarade001 <maheshsarade001@gmail.com>
2024-02-08 00:16:28 +0530

    Input and Button component added

---
+++ src/components/Button.jsx
@@ -0,0 +1,24 @@
+import classNames from "classnames";
+import PropTypes from "prop-types";
+const Button = ({ children, className, onClick, ...props }) => {
+  return (
+    <button
+      onClick={onClick}
+      className={classNames(
+        " bg-blue-500 text-white  items-center justify-center font-medium transition-all duration-300 ease-in-out disabled:opacity-50 hover:bg-blue-600 active:bg-blue-700 border-blue-500  px-4 py-2 text-base border-none rounded-md disabled:cursor-not-allowed ",
+        className
+      )}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+};
+
+Button.propTypes = {
+  children: PropTypes.node.isRequired,
+  className: PropTypes.string,
+  onClick: PropTypes.func,
+};
+
+export default Button;
