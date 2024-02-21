bdf322433c25ec51a72f8ad56e21d05aad6d6639
maheshsarade001 <maheshsarade001@gmail.com>
2024-02-22 00:09:19 +0530

    Created new Home comp. for routing structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,16 @@
-import {
-  useJsApiLoader,
-  GoogleMap,
-  DirectionsRenderer,
-} from "@react-google-maps/api";
-import useRouteStore from "./store/route.js";
-import BookingForm from "./components/BookingForm";
-import { Toaster } from "react-hot-toast";
-import Book from "./components/Book.jsx";
+import React from "react";
+import Home from "./components/Home";
+import SignUp from "./components/SignUp";
+import SignIn from "./components/SignIn";
 
-const center = { lat: 18.4669, lng: 73.7766 };
-const libraries = ["places"];
-
-function App() {
-  const { direction } = useRouteStore((state) => ({
-    direction: state.direction,
-  }));
-
-  const { isLoaded } = useJsApiLoader({
-    googleMapsApiKey: import.meta.env.VITE_APP_MAP_API_KEY,
-    libraries: libraries,
-  });
-  if (isLoaded)
-    return (
-      <>
-        <div>
-          <Toaster />
-          <div className="absolute w-screen h-screen top-0 left-0">
-            <GoogleMap
-              center={center}
-              zoom={15}
-              mapContainerStyle={{ width: "100%", height: "100%" }}
-              options={{
-                zoomControl: false,
-                streetViewControl: false,
-                mapTypeControl: false,
-                fullscreenControl: false,
-              }}
-            >
-              {direction && <DirectionsRenderer directions={direction} />}
-            </GoogleMap>
-          </div>
-          <BookingForm />
-          <Book />
-        </div>
-      </>
-    );
-}
+const App = () => {
+  return (
+    <div>
+      {/* <Home /> */}
+      <SignUp />
+      <SignIn />
+    </div>
+  );
+};
 
 export default App;
---
+++ src/components/Home.jsx
@@ -0,0 +1,50 @@
+import {
+  useJsApiLoader,
+  GoogleMap,
+  DirectionsRenderer,
+} from "@react-google-maps/api";
+import useRouteStore from "../store/route.js";
+import BookingForm from "./BookingForm.jsx";
+import { Toaster } from "react-hot-toast";
+import Book from "./Book.jsx";
+
+const center = { lat: 18.4669, lng: 73.7766 };
+const libraries = ["places"];
+
+const Home = () => {
+  const { direction } = useRouteStore((state) => ({
+    direction: state.direction,
+  }));
+
+  const { isLoaded } = useJsApiLoader({
+    googleMapsApiKey: import.meta.env.VITE_APP_MAP_API_KEY,
+    libraries: libraries,
+  });
+  if (isLoaded)
+    return (
+      <>
+        <div>
+          <Toaster />
+          <div className="absolute w-screen h-screen top-0 left-0">
+            <GoogleMap
+              center={center}
+              zoom={15}
+              mapContainerStyle={{ width: "100%", height: "100%" }}
+              options={{
+                zoomControl: false,
+                streetViewControl: false,
+                mapTypeControl: false,
+                fullscreenControl: false,
+              }}
+            >
+              {direction && <DirectionsRenderer directions={direction} />}
+            </GoogleMap>
+          </div>
+          <BookingForm />
+          <Book />
+        </div>
+      </>
+    );
+};
+
+export default Home;
